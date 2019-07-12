92ef603e9dd250fda12fb00787af153a6f8aece0
Marcin Rudolf <marcin.rudolf@neufund.org>
2019-07-12 09:46:19 +0200

    makes simulate blocks run immediately and do not exit on error

--- scripts/simulate_blocks.sh
+++ scripts/simulate_blocks.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-sleep 30
-
 if [[ "$SIMULATE_BLOCKS" != true ]] ; then
     echo "SIMULATE_BLOCK env variable not true - exiting"
     exit 1
@@ -17,7 +15,6 @@ while [ 1 ]
     OUTPUT=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from": "0x04BEfE8ab2aB7Ce71C610A5daE0cbF826b6C4F7a", "to": "0x04BEfE8ab2aB7Ce71C610A5daE0cbF826b6C4F7a", "gas": "0x55F0", "gasPrice": "0x989680", "value": "0x00", "data": "0x00"}],"id":1}' http://localhost:8545)
     if [[ ${OUTPUT} != *"result"* ]] ; then
       echo "Transaction didn't pass see output: $OUTPUT"
-      exit 1
     fi
     sleep ${FREQ}
 done
